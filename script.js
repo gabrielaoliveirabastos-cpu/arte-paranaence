991d1de3442fac4259311268cc89bd73df5f3bb1
gabrielaoliveirabastos-cpu <gabriela.oliveira.bastos@escola.pr.gov.br>
2026-09-09 17:50:31 -0300

    atualizei o meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const botao = document.querySelector("#botaoMensagem");
+const mensagem = document.querySelector("#mensagemIntera")
+botao.addEvenListener("click", mostrarMostrarMensagem);
+function mostrarmensagem() {
+   mensagem.textContent =
+    "Voce acbou de criar uma interacao com JAVAScri"  
 
+}
